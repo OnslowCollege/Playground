9e8825913636b00581de61ca4b803ce35114cd01
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2025-11-26 23:46:06 +0000

    Add package addition instructions

--- Package.swift
+++ Package.swift
@@ -5,8 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Program",
+    // Visit https://swiftpackageindex.com/ to find relevant packages, then click
+    // on the Use This Package button on the right. Copy-paste the .package lines here,
+    // with each one separated by commands
     dependencies: [
-        // ...
+        // .package(url: "https://github.com/SomeUser/SomePackage.git", from: "0.0.1"),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
